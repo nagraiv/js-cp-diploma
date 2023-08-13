446c8dea237f38207175414c6d644a7ff07bda74
GraN <117268180+nagraiv@users.noreply.github.com>
2023-08-13 15:54:59 +0700

    timestamps is added

--- js/cinema.js
+++ js/cinema.js
@@ -8,10 +8,19 @@ function getClientLocale() {
     return (navigator && navigator.language) || 'ru-RU';
 }
 
+/**
+ * Возвращает день недели в коротком формате и на выбранном языке
+ * date - дата
+ * */
 function getWeekday( date=new Date(), locale=getClientLocale() ) {
     return date.toLocaleString( locale, { weekday: 'short' } );
 }
 
+/**
+ * Склоняет слово "минута" в соответствии с
+ * количественным числительным
+ * quantity - количество минут
+ * */
 function pluralizeMinutes( quantity ) {
     const rusPluralize = new Intl.PluralRules('ru-RU');
     const pluralize = (quantity, ...nounCase) => {
@@ -25,22 +34,38 @@ function pluralizeMinutes( quantity ) {
     return pluralize(quantity, 'минута', 'минуты', 'минут');
 }
 
+/**
+ * Класс Cinema инкапсулирует логику главной страницы сайта
+ * управляет календарём и расписанием фальмов
+ * */
 class Cinema {
     constructor() {
+        // запоминаем дату на начало дня в формате timestamp
+        this.date = new Date().setHours(0,0,0,0);
         this.calendar = document.querySelector('.page-nav');
         this.calendar.addEventListener('click', this.selectDay.bind(this));
-        this.fillCalendar();
+        this.fillCalendar(this.date);
 
         this.movieContainer = document.querySelector('main');
-        this.renderSchedule();
+        this.renderSchedule(this.date);
     }
 
-    renderSchedule() {
+    /**
+     * С помощью ApiRequest.getSchedule получает с сервера
+     * полную информацию о фильмах, залах и сеансах,
+     * перерисовывает основное содержимое страницы (main)
+     * timestamp - текущаяя дата в формате timestamp
+     * сохраняет информацию о залах в сессионном хранилище
+     * */
+    renderSchedule( timestamp ) {
+        timestamp = Math.floor( timestamp / 1000 ).toString();
         ApiRequest.getSchedule( (response) => {
             console.log(response);
+            sessionStorage.setItem('halls', JSON.stringify( response.halls.result ));
+
+            this.movieContainer.dataset.date = timestamp;
             let html = ''
             response.films.result.forEach(movie => html += this.getMovieHTML( movie, response ));
-            // console.log(html);
             this.movieContainer.innerHTML = html;
         } );
     }
@@ -104,19 +129,20 @@ class Cinema {
      * Формирует HTML-код div-элемента с расписанием
      * сеансов конкретного фильма в конкретном зале.
      * hall - объект c информацией о зале
+     * movieId - id фильма
      * response - ответ сервера с данными по всем фильмам, залам, сеансам
      * */
-    getHallHTML(  hall, movieId, response  ) {
+    getHallHTML(  hall, movieId, response ) {
         const seances = response.seances.result
             .filter(item => item.seance_filmid === movieId && item.seance_hallid === hall.hall_id)
             // сортируем сеансы по времени начала
             .sort((a, b) => a.seance_time > b.seance_time);
         // console.log('Сеансы: ', seances);
-        let html = `<div class="movie-seances__hall">
+        let html = `<div class="movie-seances__hall data-hall_id="${ hall.hall_id }">
         <h3 class="movie-seances__hall-title">${ hall.hall_name }</h3>
         <ul class="movie-seances__list">
         `;
-        seances.forEach(seance => html += this.getSeanceHTML( seance ));
+        seances.forEach(seance => html += this.getSeanceHTML( seance, hall.hall_id, movieId ));
         return html + `</ul>
             </div>
         `;
@@ -125,11 +151,16 @@ class Cinema {
     /**
      * Формирует HTML-код одного сеанса
      * seance - объект c информацией о сеансе
+     * hallId - id зала
+     * movieId - id фильма
      * */
-    getSeanceHTML( seance ) {
-        return `<li class="movie-seances__time-block">
-            <a class="movie-seances__time" href="html/hall.html">${ seance.seance_time }</a>
-            </li>
+    getSeanceHTML( seance, hallId, movieId ) {
+        const timestamp = parseInt(seance.seance_start) * 60 + parseInt(this.movieContainer.dataset.date);
+        return `<li class="movie-seances__time-block data-seance_id="${ seance.seance_id }">
+            <a class="movie-seances__time" 
+        href="html/hall.html?movieId=${ movieId }&hallId=${ hallId }&seanceId=${ seance.seance_id }&timestamp=${ timestamp }">
+        ${ seance.seance_time }</a>
+        </li>
         `;
     }
 
@@ -147,15 +178,16 @@ class Cinema {
             previousChosenDay?.classList.remove('page-nav__day_chosen');
             newChosenDay.classList.add('page-nav__day_chosen');
 
-            this.renderSchedule();
+            this.renderSchedule( newChosenDay.dataset.date * 1000 );
         }
     }
 
     /**
      * Заполняет календарь в навигационной панели для посмотра расписания сеансов
+     * timestamp - дата в формате timestamp, по умолчанию - сегодня
      * */
-    fillCalendar() {
-        this.calendar.innerHTML = this.getCalendarHTML( this.getDaysArray() );
+    fillCalendar( timestamp=Date.now() ) {
+        this.calendar.innerHTML = this.getCalendarHTML( this.getDaysArray( new Date( timestamp ) ) );
     }
 
     /**
@@ -173,6 +205,7 @@ class Cinema {
                 monthDay: currentDay.getDate(),
                 isToday: ind === todayInd,
                 isChosen: ind === chosenInd,
+                timestamp: currentDay.setHours(0, 0, 0, 0),
             };
             days.push( day );
             // прибавляем один день
@@ -183,14 +216,14 @@ class Cinema {
 
     /**
      * Формирует HTML-код одного дня в календаре.
-     * day - объект вида { weekday, day, isToday, isChosen }
+     * day - объект вида { weekday, day, isToday, isChosen, timestamp }
      * */
     getDayHTML( day={} ) {
         return `<a class="page-nav__day 
                 ${day.isToday ? 'page-nav__day_today' : ''} 
                 ${day.isChosen ? 'page-nav__day_chosen' : ''} 
                 ${day.weekday === 'Сб' || day.weekday === 'Вс' ? 'page-nav__day_weekend' : ''}
-                " href="#">
+                " href="#" data-date="${ Math.floor(day.timestamp / 1000).toString() }">
                     <span class="page-nav__day-week">${day.weekday}</span>
                     <span class="page-nav__day-number">${day.monthDay}</span>
                 </a>`;
@@ -198,7 +231,7 @@ class Cinema {
 
     /**
      * Формирует HTML-код всего календаря.
-     * days - массив объектов вида { weekday, day, isToday, isChosen }
+     * days - массив объектов вида { weekday, day, isToday, isChosen, timestamp }
      * для каждого элемента массива вызывает функцию getDayHTML
      * */
     getCalendarHTML( days=[] ) {
